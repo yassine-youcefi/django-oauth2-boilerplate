3f71c771c3548cfeec97d431ae52d605a6a9783b
Yani <mohamed.youcefi.etu@univ-mosta.dz>
2023-09-15 15:44:32 +0400

    update views

--- connect/views.py
+++ connect/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import GetConnectUserSerializer
-from django.shortcuts import get_object_or_404, get_list_or_404
 
 # Create your views here.
 
